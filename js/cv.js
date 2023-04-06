36e2650d5cd4feb54ac485ae8fd0bc9a7c981c6a
cybernester <mykola@cybernester.com>
2023-04-06 18:32:25 +0100

    cybernester v1.1.1 hotfix
    Adding pdf.js as the default PDF embed viewer to cv.html

--- js/cv.js
+++ js/cv.js
@@ -1,9 +1,7 @@
 function setPdfViewerHeight() {
     const pdfViewer = document.getElementById('pdfViewer');
-    const pdfViewerAlt = document.getElementById('pdfViewerAlt');
     const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     pdfViewer.style.height = `${viewportHeight * 0.9}px`;
-    pdfViewerAlt.style.height = `${viewportHeight * 0.9}px`;
 }
 
 // Call the function initially and on window resize
