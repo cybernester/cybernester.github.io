c2900b093eb73723a823ec5350ef00594aaf5239
cybernester <mykola@cybernester.com>
2023-04-06 04:44:38 +0100

    cybernester v1.1
    Fixed PDF viewer not showing on mobile at cv.html

--- js/cv.js
+++ js/cv.js
@@ -1,9 +1,12 @@
 function setPdfViewerHeight() {
     const pdfViewer = document.getElementById('pdfViewer');
+    const pdfViewerAlt = document.getElementById('pdfViewerAlt');
     const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     pdfViewer.style.height = `${viewportHeight * 0.9}px`;
+    pdfViewerAlt.style.height = `${viewportHeight * 0.9}px`;
 }
 
 // Call the function initially and on window resize
 setPdfViewerHeight();
 window.addEventListener('resize', setPdfViewerHeight);
+
