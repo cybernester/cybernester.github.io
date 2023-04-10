33b6d9f0ee6b642dc74c1e78adeb7da5c5097535
cybernester <mykola@cybernester.com>
2023-04-10 02:21:52 +0100

    cybernester.com v1.2.1
    Phone button added to the index/home page

--- js/home.js
+++ js/home.js
@@ -1,3 +1,7 @@
 var emailBtn = document.getElementById('email-btn');
-var email = atob("Y29udGFjdEBjeWJlcm5lc3Rlci5jb20="); // Decode the email address from the Base64 string in the script
-emailBtn.setAttribute('href', 'mailto:' + email); // Set the href attribute value of the email button to the decoded email address
+var email = atob("Y29udGFjdEBjeWJlcm5lc3Rlci5jb20=");
+emailBtn.setAttribute('href', 'mailto:' + email);
+
+var callBtn = document.getElementById('call-btn');
+var call = atob("KzQ0Nzk1MDk5OTc4Nw==");
+callBtn.setAttribute('href', 'tel:' + call);
